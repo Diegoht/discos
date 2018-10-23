45521304a20c8538bbbec260761e4fe6d7a89444
alumno Secundaria <al3201@cedi.local>
2018-10-23 09:48:41 -0500

    Subir a GitHub por primera vez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,22 @@
+import { HowToSaveTracksPage } from './../pages/how-to-save-tracks/how-to-save-tracks';
+import { HowToSaveReviewPage } from './../pages/how-to-save-review/how-to-save-review';
+import { HowToSaveBioPage } from './../pages/how-to-save-bio/how-to-save-bio';
+import { HowToSaveCoverPage } from './../pages/how-to-save-cover/how-to-save-cover';
+import { NightVTracksPage } from './../pages/night-v-tracks/night-v-tracks';
+import { NightVReviewPage } from './../pages/night-v-review/night-v-review';
+import { NightVBioPage } from './../pages/night-v-bio/night-v-bio';
+import { NightVCoverPage } from './../pages/night-v-cover/night-v-cover';
+import { ThrillTracksPage } from './../pages/thrill-tracks/thrill-tracks';
+import { ThrillReviewPage } from './../pages/thrill-review/thrill-review';
+import { ThrillBioPage } from './../pages/thrill-bio/thrill-bio';
+import { ThrillCoverPage } from './../pages/thrill-cover/thrill-cover';
+import { MelodramaTracksPage } from './../pages/melodrama-tracks/melodrama-tracks';
+import { MelodramaReviewPage } from './../pages/melodrama-review/melodrama-review';
+import { MelodramaCoverPage } from './../pages/melodrama-cover/melodrama-cover';
+import { DivideTracksPage } from './../pages/divide-tracks/divide-tracks';
+import { DivideReviewPage } from './../pages/divide-review/divide-review';
+import { DivideBioPage } from './../pages/divide-bio/divide-bio';
+import { DivideCoverPage } from './../pages/divide-cover/divide-cover';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -6,11 +25,32 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MelodramaBioPage } from '../pages/melodrama-bio/melodrama-bio';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DivideCoverPage,
+    DivideBioPage,
+    DivideReviewPage,
+    DivideTracksPage,
+    MelodramaCoverPage,
+    MelodramaBioPage,
+    MelodramaReviewPage,
+    MelodramaTracksPage,
+    ThrillCoverPage,
+    ThrillBioPage,
+    ThrillReviewPage,
+    ThrillTracksPage,
+    NightVCoverPage,
+    NightVBioPage,
+    NightVReviewPage,
+    NightVTracksPage,
+    HowToSaveCoverPage,
+    HowToSaveBioPage,
+    HowToSaveReviewPage,
+    HowToSaveTracksPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +59,28 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DivideCoverPage,
+    DivideBioPage,
+    DivideReviewPage,
+    DivideTracksPage,
+    MelodramaCoverPage,
+    MelodramaBioPage,
+    MelodramaReviewPage,
+    MelodramaTracksPage,
+    ThrillCoverPage,
+    ThrillBioPage,
+    ThrillReviewPage,
+    ThrillTracksPage,
+    NightVCoverPage,
+    NightVBioPage,
+    NightVReviewPage,
+    NightVTracksPage,
+    HowToSaveCoverPage,
+    HowToSaveBioPage,
+    HowToSaveReviewPage,
+    HowToSaveTracksPage
+
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,25 @@
+import { HowToSaveReviewPage } from './../how-to-save-review/how-to-save-review';
+import { HowToSaveTracksPage } from './../how-to-save-tracks/how-to-save-tracks';
+import { HowToSaveBioPage } from './../how-to-save-bio/how-to-save-bio';
+import { HowToSaveCoverPage } from './../how-to-save-cover/how-to-save-cover';
+import { NightVReviewPage } from './../night-v-review/night-v-review';
+import { NightVTracksPage } from './../night-v-tracks/night-v-tracks';
+import { NightVBioPage } from './../night-v-bio/night-v-bio';
+import { NightVCoverPage } from './../night-v-cover/night-v-cover';
+import { ThrillReviewPage } from './../thrill-review/thrill-review';
+import { ThrillTracksPage } from './../thrill-tracks/thrill-tracks';
+import { ThrillCoverPage } from './../thrill-cover/thrill-cover';
+import { MelodramaReviewPage } from './../melodrama-review/melodrama-review';
+import { MelodramaTracksPage } from './../melodrama-tracks/melodrama-tracks';
+import { MelodramaCoverPage } from './../melodrama-cover/melodrama-cover';
+import { DivideReviewPage } from './../divide-review/divide-review';
+import { DivideTracksPage } from './../divide-tracks/divide-tracks';
+import { DivideBioPage } from './../divide-bio/divide-bio';
+import { DivideCoverPage } from './../divide-cover/divide-cover';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MelodramaBioPage } from '../melodrama-bio/melodrama-bio';
+import { ThrillBioPage } from '../thrill-bio/thrill-bio';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +27,98 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  foto1=MelodramaCoverPage;
+  artist1=MelodramaBioPage;
+  tracks1=MelodramaTracksPage;
+  cosas1=MelodramaReviewPage;
+
+  foto2=DivideCoverPage;
+  artist2=DivideBioPage;
+  tracks2=DivideTracksPage;
+  cosas2=DivideReviewPage;
+
+  foto3=ThrillCoverPage;
+  artist3=ThrillBioPage;
+  tracks3=ThrillTracksPage;
+  cosas3=ThrillReviewPage;
+
+  foto4=NightVCoverPage;
+  artist4=NightVBioPage;
+  tracks4=NightVTracksPage;
+  cosas4=NightVReviewPage;
+
+  foto5=HowToSaveCoverPage;
+  artist5=HowToSaveBioPage;
+  tracks5=HowToSaveTracksPage;
+  cosas5=HowToSaveReviewPage;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  imagen1(){
+    this.navCtrl.push(this.foto1);
+  }
+  artista1(){
+    this.navCtrl.push(this.artist1);
+  }
+  canciones1(){
+    this.navCtrl.push(this.tracks1);
+  }
+  comments1(){
+    this.navCtrl.push(this.cosas1);
+  }
+
+  imagen2(){
+    this.navCtrl.push(this.foto2);
+  }
+  artista2(){
+    this.navCtrl.push(this.artist2);
+  }
+  canciones2(){
+    this.navCtrl.push(this.tracks2);
+  }
+  comments2(){
+    this.navCtrl.push(this.cosas2);
+  }
+
+  imagen3(){
+    this.navCtrl.push(this.foto3);
+  }
+  artista3(){
+    this.navCtrl.push(this.artist3);
+  }
+  canciones3(){
+    this.navCtrl.push(this.tracks3);
+  }
+  comments3(){
+    this.navCtrl.push(this.cosas3);
+  }
+
+  imagen4(){
+    this.navCtrl.push(this.foto4);
+  }
+  artista4(){
+    this.navCtrl.push(this.artist4);
+  }
+  canciones4(){
+    this.navCtrl.push(this.tracks4);
+  }
+  comments4(){
+    this.navCtrl.push(this.cosas4);
+  }
+
+  imagen5(){
+    this.navCtrl.push(this.foto5);
+  }
+  artista5(){
+    this.navCtrl.push(this.artist5);
+  }
+  canciones5(){
+    this.navCtrl.push(this.tracks5);
+  }
+  comments5(){
+    this.navCtrl.push(this.cosas5);
   }
 
 }
